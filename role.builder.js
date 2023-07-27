977cabbdb9ac1e80945d49ebeb12a42609125e5b
EdwardChen1111 <asus960324@gmail.com>
2023-07-28 01:04:23 +0800

    builder fix001

--- role.builder.js
+++ role.builder.js
@@ -5,9 +5,9 @@ let roleBuilder = {
         
         if (creep.store.getFreeCapacity() == creep.store.getCapacity() && storage != '' && creep.withdraw(storage[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
             target = storage[0];
-        } else if (bui != '' && creep.build(bui[0]) == ERR_NOT_IN_RANGE) {
+        } else if (controllertime > 5000 && bui != '' && creep.build(bui[0]) == ERR_NOT_IN_RANGE) {
             target = bui[0];
-        } else if (bui == '' && creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
+        } else if ((controllertime < 5000 || bui == '') && creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
             target = controller;
         }
         
