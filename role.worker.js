634e7c063919652656a87a024243fffcf40c8052
chanaaaaaaa <king960129@gmail.com>
2023-07-28 00:57:51 +0700

    Delete role.worker.js

--- role.worker.js
+++
@@ -1,9 +0,0 @@
-let roleWorker = {
-    run: function (creep) {
-        sources = Game.getObjectById(creep.memory.sourceID);
-        if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
-        }
-    }
-};
-module.exports = roleWorker;
