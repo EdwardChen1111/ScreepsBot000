dc9d9530df207a0aa68f4a137cea52bc10cd7322
chanaaaaaaa <king960129@gmail.com>
2023-07-28 00:58:16 +0700

    Create role.tower.js

---
+++ try/role.tower.js
@@ -0,0 +1,18 @@
+let roleTower = {
+    work: function(tower) {
+        let fight = tower.room.find(FIND_HOSTILE_CREEPS, {filter: (creep) => {return (creep.owner.username != 'EdwardChen1111')}});
+        let fix = tower.room.find(FIND_STRUCTURES, {filter: (structure) => {return (structure.structureType != STRUCTURE_WALL) && structure.hits < 250000 && structure.hits < structure.hitsMax}});
+        let doct = tower.room.find(FIND_MY_CREEPS, {filter: object => object.hits < object.hitsMax});
+        if (fight.length > 0) {
+            fight.sort((a,b) => a.hits - b.hits);
+            tower.attack(fight[fight.length - 1]);
+        } else if(doct.length > 0) {
+            doct.sort((a,b) => a.hits - b.hits);
+            tower.heal(doct[doct.length - 1]);
+        } else if(fix.length > 0) {
+            fix.sort((a,b) => a.hits - b.hits);
+            tower.repair(fix[0]);
+        }
+	}
+};
+module.exports = roleTower;
