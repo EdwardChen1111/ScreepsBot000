7ef2f53631a8878061e9f697633e581628e37bb2
chanaaaaaaa <king960129@gmail.com>
2023-07-28 01:03:49 +0700

    Update role.claimer.js

--- try/role.claimer.js
+++ try/role.claimer.js
@@ -2,16 +2,9 @@ let roleClaimer = {
     run: function (creep) {
         if (creep.room.name == creep.memory.troom){
             let controller = creep.room.controller;
-            if (controller.reservation == undefined || controller.reservation.username == 'Chenwu') {
-                if (creep.reserveController(controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(controller);
-                }
-            } 
-            else if (controller.reservation != undefined && controller.reservation.username != 'Chenwu') {
-                if(creep.attackController(controller) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(controller);
-                }
-            } 
+            if (creep.reserveController(controller) == ERR_NOT_IN_RANGE || (controller.reservation != undefined && controller.reservation.username != 'Chenwu' && creep.attackController(controller) == ERR_NOT_IN_RANGE)) {
+                creep.moveTo(controller);
+            }
         } else {
             const exitDir = creep.room.findExitTo(creep.memory.troom);
             const exit = creep.pos.findClosestByRange(exitDir);
