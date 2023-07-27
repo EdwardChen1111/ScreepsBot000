5a653904c463ce311a600577cc7fdeefc7e3fd65
chanaaaaaaa <king960129@gmail.com>
2023-07-28 01:03:45 +0700

    Update out G.js

--- out G.js
+++ out G.js
@@ -12,6 +12,22 @@ var roleCarrier = {
             else if(creep.name == 'E11S35_Harvester1'){
                 creep.memory.role = 'outH';
             }
+            else if(creep.name == 'E12S36_outAll1' || creep.name == 'E11S22_outC'){
+                if(creep.room.name == 'E10S35'){
+                    creep.memory.troom = 'E10S23';
+                }
+                if(creep.room.name == 'E10S23'){
+                    creep.memory.troom = 'E11S22';
+                }
+                if(creep.room.name == 'E11S22'){
+                    if(creep.name == 'E12S36_outAll1'){
+                        creep.memory.role = 'outAll';
+                    }
+                    else if(creep.name == 'E11S22_outC1'){
+                        creep.memory.role = 'outC';
+                    }
+                }
+            }
         }
 	}
 };
