364a08cd75ea1ba2acaf15117e45cff81b84fc93
chanaaaaaaa <king960129@gmail.com>
2023-07-31 00:28:59 +0700

    Create role.claimer.js

---
+++ creeps/role.claimer.js
@@ -0,0 +1,15 @@
+let roleClaimer = {
+    run: function (creep) {
+        if (creep.room.name == creep.memory.troom){
+            let controller = creep.room.controller;
+            if (creep.reserveController(controller) == ERR_NOT_IN_RANGE || (controller.reservation != undefined && controller.reservation.username != 'Chenwu' && creep.attackController(controller) == ERR_NOT_IN_RANGE)) {
+                creep.moveTo(controller);
+            }
+        } else {
+            const exitDir = creep.room.findExitTo(creep.memory.troom);
+            const exit = creep.pos.findClosestByRange(exitDir);
+            creep.moveTo(exit);
+        }
+    }
+};
+module.exports = roleClaimer;
