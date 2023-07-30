d232102794cbe6086cc35504888e8a1fa89de786
chanaaaaaaa <king960129@gmail.com>
2023-07-30 23:07:18 +0700

    Update role.link.js

--- try/role.link.js
+++ try/role.link.js
@@ -1,7 +1,16 @@
 var roleLink = {
     work: function(link) {
-        const linkFrom = Game.rooms['E12S36'].lookForAt('structure', link.pos.x, link.pos.y)[0];
-        const linkTo = Game.rooms['E12S36'].lookForAt('structure', 22, 14)[0];
+        name = link.room.name;
+        let linkFrom = '';
+        let linkTo = '';
+        if(name == 'E12S36'){
+            linkFrom = Game.rooms['E12S36'].lookForAt('structure', link.pos.x, link.pos.y)[0];
+            linkTo = Game.rooms['E12S36'].lookForAt('structure', 22, 14)[0];
+        }
+        else if(name == 'E11S35'){
+            linkFrom = Game.rooms['E11S35'].lookForAt('structure', link.pos.x, link.pos.y)[0];
+            linkTo = Game.rooms['E11S35'].lookForAt('structure', 25, 36)[0];
+        }
         linkFrom.transferEnergy(linkTo);
     }
 };
