47da26fa33cf64a5f47899c072d81cc9f237bff9
EdwardChen1111 <asus960324@gmail.com>
2023-08-01 00:11:21 +0800

    bug fix

--- role.worker.js
+++ role.worker.js
@@ -1,6 +1,6 @@
 let roleWorker = {
     run: function (creep) {
-        sources = Game.getObjectById(creep.memory.sourceID);
+        let sources = Game.getObjectById(creep.memory.sourceID);
         if (creep.harvest(sources) == ERR_NOT_IN_RANGE) {
             creep.moveTo(sources, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
